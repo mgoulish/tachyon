3f0bb51e34c7fdf22bb79006431159b76d5d93ef
mgoulish <mgoulish@redhat.com>
2020-12-09 03:05:58 -0500

    Main starts an Abstractor, which requests input from BB, then waits to receive it.

--- app/tachyon.go
+++ app/tachyon.go
@@ -4,6 +4,7 @@ package main
 
 import (
          "fmt"
+         "os"
          "time"
          
          t "tachyon"
@@ -14,17 +15,38 @@ var fp = fmt.Fprintf
 
 
 
+// The Abstractor sends an input request informing the BB 
+// of what data type it's interested in, and providing the
+// channel that the BB can use to send Abstractions of that
+// type to it. Then it starts listening on that channel.
+func abstractor ( name string, bb_channel t.Message_Channel ) {
+  input_channel := make ( t.Message_Channel, 5 )
 
+  input_request := t.Message { Type : "input_request",
+                               Data : map[string]interface{} { "type"    : "image",
+                                                               "channel" : input_channel} }
+  fp ( os.Stdout, "Abstractor %s sending input request.\n", name )
+  bb_channel <- input_request
+
+  fp ( os.Stdout, "Abstractor %s listening for input.\n", name )
+  for {
+    image := <- input_channel
+    fp ( os.Stdout, "Abstractor %s received image: |%#v|\n", image )
+  }
+}
+
+
+
+
+
+// Main starts the Bulletin Board and then starts one Abstractor,
+// passing it the BB's channel.
 func main ( ) {
-
-  msg := t.Message { Type : "my_type", 
-                     Data : map[string]interface{} { "Hello" : 12, "AI" : 12 } }
-
   bb_channel := t.Start_Bulletin_Board ( )
+  go abstractor ( "a1", bb_channel )
 
   for {
-    bb_channel <- msg
-    time.Sleep ( 2 * time.Second )
+    time.Sleep ( 10 * time.Second )
   }
 }
 
